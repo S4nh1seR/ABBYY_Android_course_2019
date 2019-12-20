d878219706aa31d274dfaac9e943f688316d66dc
Alexandr <SanhiseR@mail.ru>
2019-12-21 01:48:52 +0300

    Задание 6

---
+++ homework6/app/src/main/java/abbyy/com/homework6/DetailedNoteActivity.java
@@ -0,0 +1,14 @@
+package abbyy.com.homework6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailedNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_note);
+
+    }
+}
