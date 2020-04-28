d46ca884ebe9fc872f5b6f70ac2b5b3fbe9c7bf9
Alexandr <SanhiseR@mail.ru>
2020-04-28 10:49:44 +0300

    семестр 2 задание 1 - фрагменты 1

---
+++ homework9/app/src/main/java/abbyy/com/homework9/MainActivity.java
@@ -0,0 +1,55 @@
+package abbyy.com.homework9;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static Intent getIntent(@NonNull final Context context) {
+        return new Intent(context, MainActivity.class);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.setTitle(R.string.app_name);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.mainListContainer, ListFragment.newInstance(), ListFragment.TAG)
+                    .addToBackStack(null)
+                    .commit();
+        }
+    }
+
+    public void showDetailFragment(@NonNull final long id) {
+        if (getSupportFragmentManager().findFragmentByTag(DetailFragment.TAG) != null) {
+            // Если на экране уже есть фрагмент с деталями, то надо его убрать перед показом нового
+            getSupportFragmentManager().popBackStack();
+        }
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.mainListContainer, DetailFragment.newInstance(id), DetailFragment.TAG)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getSupportFragmentManager().getBackStackEntryCount() == 1) {
+            finish();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
