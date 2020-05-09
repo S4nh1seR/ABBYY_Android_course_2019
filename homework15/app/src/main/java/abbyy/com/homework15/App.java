401b47e6a30c9295e36972c770febd57310ffb57
Alexandr <SanhiseR@mail.ru>
2020-05-10 00:39:58 +0300

    добавил дз7 - меню и диалог

---
+++ homework15/app/src/main/java/abbyy/com/homework15/App.java
@@ -0,0 +1,32 @@
+package abbyy.com.homework15;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.content.Context;
+
+public class App extends Application {
+
+    @SuppressLint("StaticFieldLeak")
+    private static Context context;
+
+    private static DatabaseHolder databaseHolder;
+    private static NoteRepository noteRepository;
+
+    @Override
+    public void onCreate()  {
+        super.onCreate();
+        context = this;
+        databaseHolder = new DatabaseHolder(context);
+        noteRepository = new NoteRepository(databaseHolder);
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+    public static DatabaseHolder getDatabaseHolder() {
+        return databaseHolder;
+    }
+    public static NoteRepository getNoteRepository() {
+        return noteRepository;
+    }
+}
